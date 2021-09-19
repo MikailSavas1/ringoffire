0fbf07801fa6e24499e23c06c095390c9767508b
MikailSavas1 <82816092+MikailSavas1@users.noreply.github.com>
2021-09-19 16:29:24 +0200

    game component & card-stack

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   newGame(){
-    console.log('start new game');
+    this.router.navigateByUrl('/game');
   }
 
 }
